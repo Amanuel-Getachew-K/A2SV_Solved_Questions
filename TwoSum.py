358b6b00d09c2640fc8c9b95f18591b07dd2acfa
Amanuel Getachew Kassa <amanget132@gmail.com>
2026-02-09 16:24:23 +0300

    Implement twoSum function in TwoSum.py

---
+++ TwoSum.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        idx = {}
+        for i,num in enumerate(nums):
+            if (target - num) in idx:
+                return [idx[target-num],i]
+            idx[num] = i
