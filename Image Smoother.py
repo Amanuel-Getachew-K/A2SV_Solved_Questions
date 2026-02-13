d6c51f466cab50e523f867148543f968a14e1586
Amanuel Getachew Kassa <amanget132@gmail.com>
2026-02-13 11:04:25 +0300

    Create Image Smoother.py

---
+++ Image Smoother.py
@@ -0,0 +1,13 @@
+class Solution:
+    def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
+        n,m = len(img),len(img[0])
+        avg = [[0] * m for _ in range(n)]
+        for i in range(n):
+            for j in range(m):
+                total = count = 0
+                for k in range(max(0,i-1), min(n,i+2)):
+                    for s in range(max(0,j-1), min(m,j+2)):
+                        total += img[k][s]
+                        count += 1
+                avg[i][j] = total // count
+        return avg
