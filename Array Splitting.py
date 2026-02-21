1cab0e1feaf4cd3ebb2359acf4f71db6106e716c
Amanuel Getachew Kassa <amanget132@gmail.com>
2026-02-21 21:05:56 +0300

    Create Array Splitting.py

---
+++ Array Splitting.py
@@ -0,0 +1,17 @@
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+
+if k == 1:
+    print(a[-1] - a[0])
+else:
+    gaps = []
+    for i in range(1, n):
+        gaps.append(a[i] - a[i - 1])
+
+    gaps.sort(reverse=True)
+
+    total = a[-1] - a[0]
+    for i in range(k - 1):
+        total -= gaps[i]
+
+    print(total)
