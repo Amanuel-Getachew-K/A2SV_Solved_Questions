0823af210e3abfc1034ba301bb7cbc17bfadfbe6
Amanuel Getachew Kassa <amanget132@gmail.com>
2026-03-06 19:46:36 +0300

    Add solution for Subarray Sums Divisible by K

--- leetcode/0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
+++ leetcode/0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
@@ -1,14 +1,14 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        count = 0
         exist = {0:1}
         total = 0
-        for n in nums:
-            total += n
+        answer = 0
+        for num in nums:
+            total += num
             if total % k in exist:
-                count += exist[total % k]
+                answer += exist[total % k]
                 exist[total % k] += 1
             else:
                 exist[total % k] = 1
 
-        return count
+        return answer
